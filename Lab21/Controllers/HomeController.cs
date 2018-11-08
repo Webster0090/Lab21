63c4cc377c72c4e96f2d54535051916aa866ef8b
Steve Webster <steve.webster190@gmail.com>
2018-11-07 19:23:59 -0500

    Add project files.

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Summary()
+        {
+            return View();
+        }
+        public ActionResult AddUser(Register newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.WelcomeMessage = $"Welcome {newUser.FirstName}!";
+                return View("Summary");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ Lab21/Models/Register.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21.Models
+{
+    public class Register
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([0-9A-z]{5,30})(@[0-9A-z]{5,10}).([0-9A-z]{2,3})$")]
+        public string emailaddress { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\d{3})-(\d{3})-(\d{4})$")]
+        public string PhoneNumber { get; set; }
+        
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]\w{3,14}$")]
+        public string Password { get; set; }
+
+        public Register()
+        {
+            FirstName = "";
+            LastName = "";
+            emailaddress = "";
+            PhoneNumber = "";
+            Password = "";
+        }
+        public Register(string fn, string ln, string ea, string pn, string pw)
+        {
+            FirstName = fn;
+            LastName = ln;
+            emailaddress = ea;
+            PhoneNumber = pn;
+            Password = pw;
+        }
+
+
+
+
+
+
+
+
+    }
+}
